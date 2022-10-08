a00b7f2ceae4f9ab4f2acf6c251ed0225164f72f
Asier Alba <asikwondo@gmail.com>
2022-10-08 11:00:24 +0200

    Abstracted to constants the response message

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,18 +4,22 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class FizzBuzz
 {
+    const FIZZ_BUZZ = "FizzBuzz";
+    const FIZZ = "Fizz";
+    const BUZZ = "Buzz";
+
     public function calculate(string $number): string
     {
         if($number % 3 == 0 && $number % 5 == 0) {
-            return "FizzBuzz";
+            return self::FIZZ_BUZZ;
         }
 
         if($number % 3 == 0 || str_contains($number, "3")) {
-            return "Fizz";
+            return self::FIZZ;
         }
 
         if($number % 5 == 0 || str_contains($number, "5")) {
-            return "Buzz";
+            return self::BUZZ;
         }
 
         return $number;
