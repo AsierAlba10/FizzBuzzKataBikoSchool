348247952b2ca9570def1e8f53e19d582cd6c467
Asier Alba <asikwondo@gmail.com>
2022-10-08 11:15:31 +0200

    Abstracted two methods to get a rule of a divisible number

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -10,18 +10,33 @@ class FizzBuzz
 
     public function calculate(string $number): string
     {
-        if($number % 3 == 0 && $number % 5 == 0) {
+        if($this->divisibleByTwoNumbers($number)) {
             return self::FIZZ_BUZZ;
         }
 
-        if($number % 3 == 0 || str_contains($number, "3")) {
+        if($this->divisibleByThree($number) || str_contains($number, "3")) {
             return self::FIZZ;
         }
 
-        if($number % 5 == 0 || str_contains($number, "5")) {
+        if($this->divisibleByFive($number) || str_contains($number, "5")) {
             return self::BUZZ;
         }
 
         return $number;
+    }
+
+    private function divisibleByTwoNumbers(string $numberToAnalyze): bool
+    {
+        return $numberToAnalyze % 3 == 0 && $numberToAnalyze % 5 == 0;
+    }
+
+    private function divisibleByThree(string $numberToAnalyze): bool
+    {
+        return $numberToAnalyze % 3 == 0;
+    }
+
+    private function divisibleByFive(string $numberToAnalyze): bool
+    {
+        return $numberToAnalyze % 5 == 0;
     }
 }
